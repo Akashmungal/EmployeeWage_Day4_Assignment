661d862e92cec7f8b70c8a5d46bc0ba32874a3ce
Akashmungal <mungalakash92@gmail.com>
2022-03-08 01:23:33 +0530

    calculate Wage for a month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,33 +3,42 @@ package com.bridgelabz;
 public class EmployeeWage {
 
     public static final int WAGE_PER_HR = 20;
-    public static final int FULL_DAY_HOUR = 8;
     public static final int IS_FULL_TIME = 2;
     public static final int IS_PART_TIME = 1;
+    public static final int TOTAL_WORKING_DAY_IN_MONTH = 20;
+    public static final int TOTAL_WORKING_HRS = 100;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
 
-        int dailyEmpWage = 0;
-        int empHrs = 0;
+        int workingdays = 0;
+        int wageForMonth = 0;
+        int totalEmpHrs = 0;
+        int empHrs;
 
-        int empCheck = (int) (Math.floor (Math.random () * 10) % 3);
+        while (totalEmpHrs < TOTAL_WORKING_HRS && workingdays < TOTAL_WORKING_DAY_IN_MONTH ) {
+            workingdays++;
 
-        switch (empCheck) {
+            int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
 
-            case 1:
-                empHrs = 4;
-                break;
+            switch (empCheck) {
 
-            case 2:
-                empHrs = 8;
-                break;
+                case 1:
+                    empHrs = 4;
+                    break;
 
-            default:
-                empHrs = 0;
+                case 2:
+                    empHrs = 8;
+                    break;
+
+                default:
+                    empHrs = 0;
+            }
+
+            totalEmpHrs = totalEmpHrs + empHrs;
         }
 
-        dailyEmpWage = empHrs * WAGE_PER_HR;
-        System.out.println("Employee Daily Wage= " +dailyEmpWage);
+        wageForMonth += totalEmpHrs * WAGE_PER_HR;
+        System.out.println("Employee wage in a month " + wageForMonth);
     }
 }
