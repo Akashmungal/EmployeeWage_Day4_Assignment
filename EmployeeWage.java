9d1c112324ce8d0dbb494a0c7af4b4f9e44797bc
Akashmungal <mungalakash92@gmail.com>
2022-03-08 01:20:36 +0530

    calculate daily employee wage Add part time Use Switch Case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -15,18 +15,21 @@ public class EmployeeWage {
 
         int empCheck = (int) (Math.floor (Math.random () * 10) % 3);
 
-        if(empCheck == IS_FULL_TIME) {
-            empHrs = 8;
-        }
-        else if(empCheck == IS_PART_TIME) {
-            empHrs = 4;
-        }
-        else{
-            empHrs = 0;
+        switch (empCheck) {
+
+            case 1:
+                empHrs = 4;
+                break;
+
+            case 2:
+                empHrs = 8;
+                break;
+
+            default:
+                empHrs = 0;
         }
 
         dailyEmpWage = empHrs * WAGE_PER_HR;
         System.out.println("Employee Daily Wage= " +dailyEmpWage);
-
     }
 }
